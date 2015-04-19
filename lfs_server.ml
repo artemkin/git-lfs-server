3e37aa45545461f1bad420eb53426856bd6840d7
Stanislav Artemkin <artemkin@gmail.com>
2015-04-19 23:50:21 +0400

    Added PUT requests handling

--- lfs_server.ml
+++ lfs_server.ml
@@ -59,18 +59,18 @@ module Json = struct
     Yojson.pretty_to_string msg
 
   let parse_oid_size str =
-    try_with (fun () -> return @@ Yojson.Safe.from_string str) >>= function
-    | Error _ -> return None
+    try_with (fun () -> return @@ Yojson.Safe.from_string str)
+    >>| function
+    | Error _ -> None
     | Ok (`Assoc ["oid", `String oid; "size", `Int size]) ->
       if is_sha256_hex_digest oid then
-        return (Some (oid, Int64.of_int size))
-      else return None
+        Some (oid, Int64.of_int size)
+      else None
     | Ok (`Assoc ["oid", `String oid; "size", `Intlit size]) ->
       let oid = if is_sha256_hex_digest oid then Some oid else None in
       let size = Option.try_with (fun () -> Int64.of_string size) in
-      return @@ Option.both oid size
-    | Ok _ -> return None
-
+      Option.both oid size
+    | Ok _ -> None
 end
 
 let add_content_type headers content_type =
@@ -78,23 +78,40 @@ let add_content_type headers content_type =
 
 let respond_with_string ~meth ~code str =
   let headers = add_content_type (Header.init ()) "application/vnd.git-lfs+json" in
-  let body = match meth with `GET | `POST -> `String str | `HEAD -> `Empty in
+  let body = match meth with `GET | `POST | `PUT -> `String str | `HEAD -> `Empty in
   Server.respond ~headers ~body code
 
 let respond_error ~meth ~code msg =
   respond_with_string ~meth ~code @@ Json.error msg
 
-let get_oid_path ~oid =
-  let oid02 = String.prefix oid 2 in
-  let oid24 = String.sub oid ~pos:2 ~len:2 in
-  Filename.of_parts [oid02; oid24; oid]
+let mkdir_if_needed dirname =
+  try_with (fun () -> Unix.stat dirname) >>= function
+  | Ok _ -> Deferred.unit
+  | Error _ ->
+    try_with (fun () -> Unix.mkdir dirname)
+    >>= fun _ -> Deferred.unit
+
+let get_oid_prefixes ~oid =
+  (String.prefix oid 2, String.sub oid ~pos:2 ~len:2)
+
+let mkdir_object_if_needed ~root ~oid =
+  let (oid02, oid24) = get_oid_prefixes ~oid in
+  let dir02 = Filename.of_parts [root; "/objects"; oid02] in
+  let dir24 = Filename.concat dir02 oid24 in
+  mkdir_if_needed dir02 >>= fun () ->
+  mkdir_if_needed dir24
 
 let get_object_filename ~root ~oid =
-  Filename.of_parts [root; "/objects"; get_oid_path ~oid]
+  let (oid02, oid24) = get_oid_prefixes ~oid in
+  let oid_path = Filename.of_parts [oid02; oid24; oid] in
+  Filename.of_parts [root; "/objects"; oid_path]
+
+let get_temp_filename ~root ~oid =
+  Filename.of_parts [root; "/temp"; oid]
 
 let check_object_file_stat ~root ~oid =
-  let path = get_object_filename ~root ~oid in
-  try_with (fun () -> Unix.stat path)
+  let filename = get_object_filename ~root ~oid in
+  try_with (fun () -> Unix.stat filename)
 
 let get_download_url uri oid =
   Uri.with_path uri @@ Filename.concat "/data/objects" oid
@@ -127,10 +144,10 @@ let respond_object ~root ~meth ~oid =
 let oid_from_path path =
   match String.rsplit2 path ~on:'/' with
   | Some ("/objects", oid) ->
-    if is_sha256_hex_digest oid then `Metadata oid else `Empty
+    if is_sha256_hex_digest oid then `Default_path oid else `Wrong_path
   | Some ("/data/objects", oid) ->
-    if is_sha256_hex_digest oid then `Object oid else `Empty
-  | _ -> `Empty
+    if is_sha256_hex_digest oid then `Download_path oid else `Wrong_path
+  | _ -> `Wrong_path
 
 (* TODO fix this *)
 let fix_uri port uri =
@@ -140,9 +157,9 @@ let fix_uri port uri =
 let handle_get root meth uri =
   let path = Uri.path uri in
   match oid_from_path path with
-  | `Metadata oid -> respond_object_metadata ~root ~meth ~uri ~oid
-  | `Object oid -> respond_object ~root ~meth ~oid
-  | `Empty -> respond_error ~meth ~code:`Not_found "Wrong path"
+  | `Default_path oid -> respond_object_metadata ~root ~meth ~uri ~oid
+  | `Download_path oid -> respond_object ~root ~meth ~oid
+  | `Wrong_path -> respond_error ~meth ~code:`Not_found "Wrong path"
 
 let handle_post root meth uri body =
   let path = Uri.path uri in
@@ -160,8 +177,28 @@ let handle_post root meth uri body =
       | Ok _ ->
         respond_error ~meth ~code:`Bad_request "Wrong object size"
       | Error _ ->
-        let url = Uri.with_path uri @@ Filename.concat "/upload/objects" oid in
+        let url = Uri.with_path uri @@ Filename.concat "/objects" oid in
         respond_with_string ~meth ~code:`Accepted @@ Json.upload url
+
+let handle_put root meth uri body =
+  let path = Uri.path uri in
+  match oid_from_path path with
+  | `Download_path _ | `Wrong_path ->
+    respond_error ~meth ~code:`Not_found "Wrong path"
+  | `Default_path oid ->
+    check_object_file_stat ~root ~oid >>= function
+    | Ok _ -> Server.respond `OK (* already exist *)
+    | Error _ ->
+      let filename = get_object_filename ~root ~oid in
+      let temp_file = get_temp_filename ~root ~oid in
+      mkdir_object_if_needed ~root ~oid >>= fun () ->
+      try_with (fun () ->
+          Writer.with_file_atomic ~temp_file ~fsync:true filename
+            ~f:(fun w ->
+                Pipe.transfer_id (Body.to_pipe body) (Writer.pipe w)))
+      >>= function
+      | Ok _ -> Server.respond `Created
+      | Error _ -> Server.respond `Internal_server_error
 
 let serve_client ~root ~port ~body _sock req =
   let uri = Request.uri req in
@@ -173,9 +210,14 @@ let serve_client ~root ~port ~body _sock req =
     match Request.meth req with
     | (`GET as meth) | (`HEAD as meth) -> handle_get root meth uri
     | (`POST as meth) -> handle_post root meth uri body
+    | (`PUT as meth) -> handle_put root meth uri body
     | _ -> Server.respond `Method_not_allowed
 
 let start_server ~root ~host ~port () =
+  mkdir_if_needed @@ Filename.concat root "/objects"
+  >>= fun () ->
+  mkdir_if_needed @@ Filename.concat root "/temp"
+  >>= fun () ->
   eprintf "Listening for HTTP on port %d\n" port;
   Unix.Inet_addr.of_string_or_getbyname host
   >>= fun host ->
