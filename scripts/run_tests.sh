fe967b47cb59f87d8187e5c02c2730747f7d8352
Stanislav Artemkin <artemkin@gmail.com>
2015-05-07 22:17:18 +0400

    specified explicit order of tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -18,7 +18,30 @@ sleep 2
 
 echo Run tests
 cd tests
-find . -iname '*.ml' -exec '{}' \;
+
+echo Test GET method
+./test_get_metadata.ml
+./test_get_object.ml
+./test_get_uppercase_oid_rejected.ml
+
+echo Test HEAD method
+./test_head_metadata.ml
+./test_head_object.ml
+
+./test_method_not_allowed.ml
+./test_no_host.ml
+
+echo Test POST method
+./test_post_empty_json.ml
+./test_post_existing_object.ml
+./test_post_existing_object_wrong_size.ml
+./test_post_new_object.ml
+./test_post_uppercase_oid_rejected.ml
+
+echo Test PUT method
+./test_put_1_new_file_wrong_content.ml
+./test_put_2_new_file_correct_content.ml
+./test_put_3_existing_file.ml
 
 echo Stop server
 kill $LFS_SERVER_PID
